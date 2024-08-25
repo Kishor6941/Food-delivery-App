d3fb3bcd6b9de55372b34eee3ce1bafe02e5b735
Kishor6941 <phawadeks9049@gmail.com>
2024-08-25 11:17:14 +0530

    Place-order

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -5,13 +5,15 @@ import ExploreMenu from "../../components/ExploreMenu/ExploreMenu";
 import FoodDisplay from "../../components/FoodDisplay/FoodDisplay";
 import AppDownload from "../../components/AppDownload/AppDownload";
 const Home = () => {
-    const [category, setCategory] = useState("All");
-  return <div>
-    <Header />
-    <ExploreMenu category={category} setCategory={setCategory} />
-    <FoodDisplay category={category} />
-    <AppDownload />
-  </div>;
+  const [category, setCategory] = useState("All");
+  return (
+    <div>
+      <Header />
+      <ExploreMenu category={category} setCategory={setCategory} />
+      <FoodDisplay category={category} />
+      <AppDownload />
+    </div>
+  );
 };
 
 export default Home;
